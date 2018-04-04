3a0b112d41b063212c5607d3ff85f24d2842845a
Edward Hockedy <edhockedy@hotmail.com>
2018-04-04 01:13:27 +0100

    backup 4/4

--- vis.py
+++ vis.py
@@ -98,6 +98,64 @@ def tiff_to_3d():  # http://vtk.1045678.n5.nabble.com/reconstruct-a-stack-of-TIF
     iren.Start()
 
 
+class vtkCallback():
+    def __init__(self):
+        self.xmin = 128 # CHANGE THESE FROM HARD CODED
+        self.ymin = 128
+        self.zmin = 56
+    def key_pressed_callback(self, obj, event):
+        # https://www.cs.purdue.edu/homes/cs530/code/interactor/interactor_demo.py
+        # https://www.vtk.org/Wiki/VTK/Examples/Python/Animation
+        key = obj.GetKeySym()
+        if key == "s":
+            self.actors[0].VisibilityOff()
+        elif key == 'd':
+            self.actors[0].VisibilityOn()
+        elif key == 'k':
+            self.plane.SetNormal(1, 0, 0)
+            self.xmin += 5
+            self.plane.SetOrigin(self.xmin, 128, 56)
+            #self.plane_points.SetPoint(0, self.xmin, 128, 56)
+            #self.planes.SetPoints(self.plane_points)
+            #print(self.planes.GetPoints().GetPoint(0), self.plane_points.GetPoint(0))
+            #self.planes.GetPlane(0).SetOrigin(self.xmin, 0, 0)
+            #self.planes.SetPlane(0, self.plane)
+        elif key == 'l':
+            self.plane.SetNormal(1, 0, 0)
+            self.xmin -= 5
+            self.plane.SetOrigin(self.xmin, 128, 56)
+        elif key == 'o':
+            self.plane.SetNormal(0, 1, 0)
+            self.ymin += 5
+            self.plane.SetOrigin(128, self.ymin, 56)
+        elif key == 'p':
+            self.plane.SetNormal(0, 1, 0)
+            self.ymin -= 5
+            self.plane.SetOrigin(128, self.ymin, 56)
+        elif key == 'n':
+            self.plane.SetNormal(0, 0, 1)
+            self.zmin += 5
+            self.plane.SetOrigin(128, 128, self.zmin)
+        elif key == 'm':
+            self.plane.SetNormal(0, 0, 1)
+            self.zmin -= 5
+            self.plane.SetOrigin(128, 128, self.zmin)
+        if self.xmin < 0:
+            self.xmin = 0
+        if self.xmin > 256:
+            self.xmin = 256
+        if self.ymin < 0:
+            self.ymin = 0
+        if self.ymin > 256:
+            self.ymin = 256
+        if self.zmin < 0:
+            self.zmin = 0
+        if self.zmin > 113:
+            self.zmin = 113
+
+        obj.GetRenderWindow().Render()
+
+
 def render(acts):
     ren = vtk.vtkRenderer()
     ren.SetBackground(0.329412, 0.34902, 0.427451)  # Paraview blue
@@ -113,15 +171,19 @@ def render(acts):
     # Set an user interface interactor for the render window
     iren = vtk.vtkRenderWindowInteractor()
     iren.SetRenderWindow(ren_win)
-    print(1)
+
+    cb = vtkCallback()
+    cb.actors = acts
+    cb.renderer = ren
+    iren.AddObserver("KeyPressEvent", cb.key_pressed_callback)
     # Start the initialization and rendering
-    print(2)
     iren.Initialize()
-    print(3)
+
+    #acts[0].VisibilityOff()
     ren_win.Render()
-    print(4)
     iren.Start()
-    print(5)
+    print(ren.GetNearClippingPlaneTolerance())
+    
 
 
 def load_binary_values(start=0, stop=0):
@@ -141,44 +203,115 @@ def load_binary_values(start=0, stop=0):
 
 
 def visualise_marching_cubes_2(points, num_slices, width=256, height=256):
+    # https://www.vtk.org/Wiki/VTK/Examples/Python/WriteReadVtkImageData
+    # https://www.vtk.org/Wiki/VTK/Examples/Python/vtkCutter
     img_data = vtk.vtkImageData()  # Poly data to hold information about the model
     img_data.SetDimensions(width, height, num_slices)
-    img_data.AllocateScalars(vtk.VTK_DOUBLE, 1)
+    img_data.AllocateScalars(vtk.VTK_DOUBLE, 1)  # Need this
     point_counter = 0
     for z in range(0, num_slices):
         for y in range(0, height):
             for x in range(0, width):
                 img_data.SetScalarComponentFromDouble(x, y, z, 0, points[point_counter])  # Must be double for this version of vtk (>=4.4)
-                #print(points[point_counter], img_data.GetScalarComponentAsDouble(x, y, z, 0))
-                point_counter+=1
+                point_counter += 1
 
-    mapper = vtk.vtkPolyDataMapper()
+    # planes = vtk.vtkPlanes()
+    # pp = [(128, 128, 56), (128, 128, 56), (128, 128, 56)]
+    # plane_points = vtk.vtkPoints()
+    # for i in range(0, 1):
+    #     plane_points.InsertNextPoint(pp[i])
+    # planes.SetPoints(plane_points)
 
-    mc = vtk.vtkMarchingCubes()
+    # np = [(1.0, 0.0, 0), (0.0, 1.0, 0), (0.0, 0.0, 1.0)]
+    # normal_points = vtk.vtkDoubleArray()
+    # normal_points.SetNumberOfComponents(3)
+    # normal_points.SetNumberOfTuples(1)
+    # for i in range(0, 1):
+    #     normal_points.SetTuple3(i, np[i][0], np[i][1], np[i][2])
+    # planes.SetNormals(normal_points)
+
+    plane = vtk.vtkPlane()
+    plane.SetOrigin(128, 128, 56)
+    plane.SetNormal(1.0, 0.0, 0.0)
+
+    # Skin
+    mc = vtk.vtkMarchingCubes()  # Perform marching cubes
     mc.ComputeNormalsOn()
-    mc.SetValue(0, 800)
+    mc.SetValue(0, 600)
     mc.SetInputData(img_data)
-    mc.ComputeNormalsOn()
 
-    # surface = vtk.vtkSurfaceReconstructionFilter()
-    # surface.SetInputData(delaunay.GetOutput())
+    clipper = vtk.vtkClipPolyData()
+    clipper.SetInputConnection(mc.GetOutputPort())
+    clipper.SetClipFunction(plane)
 
-    # cf = vtk.vtkContourFilter()
-    # cf.SetInputConnection(surface.GetOutputPort())
-    # cf.SetValue(0, 0.0)
+    clip_mapper = vtk.vtkPolyDataMapper()
+    clip_mapper.ScalarVisibilityOff()  # Use this to make sure shows the colour given below
+    clip_mapper.SetInputConnection(clipper.GetOutputPort())
 
-    # reverse = vtk.vtkReverseSense()
-    # reverse.SetInputConnection(cf.GetOutputPort())
-    # reverse.ReverseCellsOn()
-    # reverse.ReverseNormalsOn()
+    clip_actor = vtk.vtkActor()
+    clip_actor.SetMapper(clip_mapper)
+    clip_actor.GetProperty().SetDiffuseColor(1, 0.49, 0.25)
+    clip_actor.GetProperty().SetSpecular(0.3)
+    clip_actor.GetProperty().SetSpecularPower(20)
+    clip_actor.GetProperty().SetOpacity(0.5)
 
-    mapper.SetInputConnection(mc.GetOutputPort())
-    mapper.ScalarVisibilityOff()
+    # Bone
+    mc_bone = vtk.vtkMarchingCubes()
+    mc_bone.ComputeNormalsOn()
+    mc_bone.SetValue(0, 1500)
+    mc_bone.SetInputData(img_data)
 
-    actor = vtk.vtkActor()
-    actor.SetMapper(mapper)
-    actor.GetProperty().SetPointSize(2)
-    return actor
+    clipper_bone = vtk.vtkClipPolyData()
+    clipper_bone.SetInputConnection(mc_bone.GetOutputPort())
+    clipper_bone.SetClipFunction(plane)
+
+    clip_mapper_bone = vtk.vtkPolyDataMapper()
+    clip_mapper_bone.ScalarVisibilityOff()
+    clip_mapper_bone.SetInputConnection(clipper_bone.GetOutputPort())
+
+    clip_actor_bone = vtk.vtkActor()
+    clip_actor_bone.GetProperty().SetColor(1, 1, 0.95)
+    clip_actor_bone.SetMapper(clip_mapper_bone)
+
+    backFaces = vtk.vtkProperty()
+    backFaces.SetSpecular(0.0)
+    backFaces.SetDiffuse(0.0)
+    backFaces.SetAmbient(1.0)
+    backFaces.SetAmbientColor(0.7, 0.7, 0.65)
+ 
+    clip_actor_bone.SetBackfaceProperty(backFaces)
+
+    acts = [clip_actor, clip_actor_bone]
+    
+    # Render
+    ren = vtk.vtkRenderer()
+    ren.SetBackground(0.329412, 0.34902, 0.427451)  # Paraview blue
+
+    for act in acts:
+        ren.AddActor(act)
+
+    # Create a window for the renderer of size 500x500
+    ren_win = vtk.vtkRenderWindow()
+    ren_win.AddRenderer(ren)
+    ren_win.SetSize(500, 500)
+
+    # Set an user interface interactor for the render window
+    iren = vtk.vtkRenderWindowInteractor()
+    iren.SetRenderWindow(ren_win)
+
+    cb = vtkCallback()
+    cb.actors = acts
+    cb.renderer = ren
+    #cb.plane_points = plane_points
+    #cb.planes = planes
+    cb.plane = plane
+    iren.AddObserver("KeyPressEvent", cb.key_pressed_callback)
+    # Start the initialization and rendering
+    iren.Initialize()
+
+    ren_win.Render()
+    iren.Start()
+
 
 
 def visualise_marching_cubes(points, num_slices, width=256, height=256):
@@ -565,4 +698,4 @@ a = load_binary_values(start, stop+1)
 #act = visualise_point_cloud(a, stop-start)
 act = visualise_marching_cubes_2(a, stop-start)
 
-render([act])
+#render(act)
